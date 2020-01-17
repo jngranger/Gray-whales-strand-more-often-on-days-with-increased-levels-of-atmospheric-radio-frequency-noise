8f5cd98c7f79057d8b23a1e04bb99aef931b7444
jngranger <60012278+jngranger@users.noreply.github.com>
2020-01-17 13:00:19 -0500

    Update Functions.R

--- Functions.R
+++ Functions.R
@@ -25,12 +25,12 @@ create=function(LivedataUse,nulldata)
   nulldata$Stranded=No
   
   #Trim both datasets to only the desired columns
-  LivedataUse=LivedataUse[c("Date","MaxAP", "MaxSS", "PDO",
-                            "RadioFluxLA", "Stranded", 
+  LivedataUse=LivedataUse[c("Date","AP", "SS", "PDO",
+                            "RadioFlux", "Stranded", 
                             "DateAsCont", "RF.scale", "PDO.scale",
                             "AP.scale", "SS.scale","Year")]
-  nulldata=nulldata[c("Date","MaxAP", "MaxSS", "PDO", 
-                      "RadioFluxLA", "Stranded",
+  nulldata=nulldata[c("Date","AP", "SS", "PDO", 
+                      "RadioFlux", "Stranded",
                       "DateAsCont", "RF.scale", "PDO.scale",
                       "AP.scale", "SS.scale", "Year")]
   
@@ -53,9 +53,9 @@ create.bootstrap=function(LivedataUse,nulldata)
   randomsample$Stranded=No
   
   #Trim both datasets to only the desired columns
-  LivedataUse=LivedataUse[c("Date","MaxAP", "MaxSS", "PDO", "RadioFluxLA", "Season", "Stranded",
+  LivedataUse=LivedataUse[c("Date","AP", "SS", "PDO", "RadioFlux", "Season", "Stranded",
                             "DateAsCont", "RF.scale", "PDO.scale","AP.scale", "SS.scale","Year")]
-  randomsample=randomsample[c("Date","MaxAP", "MaxSS", "PDO", "RadioFluxLA", "Season", "Stranded",
+  randomsample=randomsample[c("Date","AP", "SS", "PDO", "RadioFlux", "Season", "Stranded",
                               "DateAsCont", "RF.scale", "PDO.scale","AP.scale", "SS.scale","Year")]
   
   #Randomly sample 600 with replacement
@@ -75,37 +75,37 @@ create.bootstrap=function(LivedataUse,nulldata)
 
 binSS=function(datause){
   #Group one =0-12
-  zero=datause[datause$MaxSS>=0,]  #Use only the data greater than 0, and less than 12
-  zero=datause[datause$MaxSS<12,]
+  zero=datause[datause$SS>=0,]  #Use only the data greater than 0, and less than 12
+  zero=datause[datause$SS<12,]
   zero.dead=sum(zero$Stranded==0)  #How many were "No Stranding"?
   zero.alive=sum(zero$Stranded==1) #How many were "Stranding"?
   G1=zero.alive/zero.dead          #Take the proportion
   #group two =12-27                #Repeat for all bins
-  one=datause[datause$MaxSS>=12,]
-  one=one[one$MaxSS<27,]
+  one=datause[datause$SS>=12,]
+  one=one[one$SS<27,]
   one.dead=sum(one$Stranded==0)
   one.alive=sum(one$Stranded==1)
   G2=one.alive/one.dead
   #Group three=27-56
-  two=datause[datause$MaxSS<56,]
-  two=two[two$MaxSS>=27,]
+  two=datause[datause$SS<56,]
+  two=two[two$SS>=27,]
   two.dead=sum(two$Stranded==0)
   two.alive=sum(two$Stranded==1)
   G3=two.alive/two.dead
   #Group four= 56-95
-  three=datause[datause$MaxSS>=56,]
-  three=three[three$MaxSS<95,]
+  three=datause[datause$SS>=56,]
+  three=three[three$SS<95,]
   three.dead=sum(three$Stranded==0)
   three.alive=sum(three$Stranded==1)
   G4=three.alive/three.dead
   #Group five= 95-151
-  four=datause[datause$MaxSS<151,]
-  four=four[four$MaxSS>=95,]
+  four=datause[datause$SS<151,]
+  four=four[four$SS>=95,]
   four.dead=sum(four$Stranded==0)
   four.alive=sum(four$Stranded==1)
   G5=four.alive/four.dead
   #Group Six=151+
-  five=datause[datause$MaxSS>=151,]
+  five=datause[datause$SS>=151,]
   five.dead=sum(five$Stranded==0)
   five.alive=sum(five$Stranded==1)
   G6=five.alive/five.dead
@@ -117,42 +117,42 @@ binSS=function(datause){
 
 #Repeat the above code but for RF
 #RF quintiles
-  #nulldata.RF=completeFun(nulldata,"RadioFluxLA")
-  #quantile(nulldata.RF$RadioFluxLA, prob = seq(0, 1, length = 7), type = 5)
+  #nulldata.RF=completeFun(nulldata,"RadioFlux")
+  #quantile(nulldata.RF$RadioFlux, prob = seq(0, 1, length = 7), type = 5)
 
 binRF=function(datause){
   #Group one =0-72
-  zero=datause[datause$RadioFluxLA>=0,]
-  zero=datause[datause$RadioFluxLA<72,]
+  zero=datause[datause$RadioFlux>=0,]
+  zero=datause[datause$RadioFlux<72,]
   zero.dead=sum(zero$Stranded==0)
   zero.alive=sum(zero$Stranded==1)
   G1=zero.alive/zero.dead
   #group two =72-82
-  one=datause[datause$RadioFluxLA>=72,]
-  one=one[one$RadioFluxLA<82,]
+  one=datause[datause$RadioFlux>=72,]
+  one=one[one$RadioFlux<82,]
   one.dead=sum(one$Stranded==0)
   one.alive=sum(one$Stranded==1)
   G2=one.alive/one.dead
   #Group three=82-100
-  two=datause[datause$RadioFluxLA<100,]
-  two=two[two$RadioFluxLA>=82,]
+  two=datause[datause$RadioFlux<100,]
+  two=two[two$RadioFlux>=82,]
   two.dead=sum(two$Stranded==0)
   two.alive=sum(two$Stranded==1)
   G3=two.alive/two.dead
   #Group four= 56-95
-  three=datause[datause$RadioFluxLA>=100,]
-  three=three[three$RadioFluxLA<126,]
+  three=datause[datause$RadioFlux>=100,]
+  three=three[three$RadioFlux<126,]
   three.dead=sum(three$Stranded==0)
   three.alive=sum(three$Stranded==1)
   G4=three.alive/three.dead
   #Group five= 95-151
-  four=datause[datause$RadioFluxLA<165,]
-  four=four[four$RadioFluxLA>=126,]
+  four=datause[datause$RadioFlux<165,]
+  four=four[four$RadioFlux>=126,]
   four.dead=sum(four$Stranded==0)
   four.alive=sum(four$Stranded==1)
   G5=four.alive/four.dead
   #Group Six=151+
-  five=datause[datause$RadioFluxLA>=165,]
+  five=datause[datause$RadioFlux>=165,]
   five.dead=sum(five$Stranded==0)
   five.alive=sum(five$Stranded==1)
   G6=five.alive/five.dead
@@ -164,42 +164,42 @@ binRF=function(datause){
 
 #Repeat the above code but for Ap
 
-#quantile((nulldata$MaxAP), prob = seq(0, 1, length = 7), type = 5)
+#quantile((nulldata$AP), prob = seq(0, 1, length = 7), type = 5)
 
 binK=function(datause)
 {
   #Group one =0-9
-  zero=datause[datause$MaxAP>=(0),]
-  zero=zero[zero$MaxAP<(9),]
+  zero=datause[datause$AP>=(0),]
+  zero=zero[zero$AP<(9),]
   zero.dead=sum(zero$Stranded==0)
   zero.alive=sum(zero$Stranded==1)
   G1=zero.alive/zero.dead
   #group two =9-15.25
-  one=datause[datause$MaxAP>=(9),]
-  one=one[one$MaxAP<(15.25),]
+  one=datause[datause$AP>=(9),]
+  one=one[one$AP<(15.25),]
   one.dead=sum(one$Stranded==0)
   one.alive=sum(one$Stranded==1)
   G2=one.alive/one.dead
   #Group three=15.25-23.875
-  two=datause[datause$MaxAP>=(15.25),]
-  two=two[two$MaxAP<(23.875),]
+  two=datause[datause$AP>=(15.25),]
+  two=two[two$AP<(23.875),]
   two.dead=sum(two$Stranded==0)
   two.alive=sum(two$Stranded==1)
   G3=two.alive/two.dead
   #Group four= 23.875-35.75
-  three=datause[datause$MaxAP>=(23.875),]
-  three=three[three$MaxAP<(35.75),]
+  three=datause[datause$AP>=(23.875),]
+  three=three[three$AP<(35.75),]
   three.dead=sum(three$Stranded==0)
   three.alive=sum(three$Stranded==1)
   G4=three.alive/three.dead
   #Group five= 35.75-54.625
-  four=datause[datause$MaxAP>=(35.75),]
-  four=four[four$MaxAP<(54.625),]
+  four=datause[datause$AP>=(35.75),]
+  four=four[four$AP<(54.625),]
   four.dead=sum(four$Stranded==0)
   four.alive=sum(four$Stranded==1)
   G5=four.alive/four.dead
   #Group Six=54.625+
-  five=datause[datause$MaxAP>=54.625,]
+  five=datause[datause$AP>=54.625,]
   five.dead=sum(five$Stranded==0)
   five.alive=sum(five$Stranded==1)
   G6=five.alive/five.dead
